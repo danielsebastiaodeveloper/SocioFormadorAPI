e225ace8a1408ec67b1988fad322b6fb760ed535
Daniel Sebastiao <daniel@mxdevelopers.net>
2023-06-08 12:20:31 -0700

    http profile and connection string without docker

--- src/Infrastructure/Persistence/SocioFormador.Persistence/Repositories/ClienteRepository.cs
+++ src/Infrastructure/Persistence/SocioFormador.Persistence/Repositories/ClienteRepository.cs
@@ -3,6 +3,8 @@ using SocioFormador.Domain.Entities;
 using SocioFormador.Domain.Interfaces;
 using SocioFormador.Domain.Interfaces.Repositories;
 using SocioFormador.Persistence.Context;
+using System.Data;
+using static Dapper.SqlMapper;
 
 namespace SocioFormador.Persistence.Repositories;
 
@@ -23,17 +25,20 @@ public class ClienteRepository : IClienteRepository
         {
             string sql = "INSERT INTO Clientes (Nombre, Apellido, Email, CiudadId, CreatedBy, CreatedAt) VALUES (@Nombre,  @Apellido, @Email, @CiudadId, @CreatedBy, @CreatedAt);";
             using var con = _dbContext.CreateConnection();
+
             var result = await con.ExecuteScalarAsync<long>(sql,
-                new
-                {
-                    @Nombre = entity.Nombre,
-                    @Apellido = entity.Apellido,
-                    @Email = entity.Email,
-                    @CiudadId = entity.CiudadId,
-                    @CreatedBy = 1,
-                    @CreatedAt = dateTimeService.NowUtc
-                });
+        new
+        {
+            @Nombre = entity.Nombre,
+            @Apellido = entity.Apellido,
+            @Email = entity.Email,
+            @CiudadId = entity.CiudadId,
+            @CreatedBy = 1,
+            @CreatedAt = dateTimeService.NowUtc
+        });
             return result;
+
+
         }, cancellationToken);
 
         return task;
